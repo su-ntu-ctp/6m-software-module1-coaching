41c829ac9d12df87000e9bc7e8f49c2cde80df40
CP Wong <38209341+cpwong@users.noreply.github.com>
2023-03-03 15:51:26 +0800

    Update higher-order-func.js

--- src/higher-order-func.js
+++ src/higher-order-func.js
@@ -1,6 +1,6 @@
-/*
-    Task 1. Outer function calculate totalPrice, inner function return a GST payable price.
-
+/*************************************************************************
+    Task 1. Outer function calculate totalPrice, 
+            inner function return a GST payable price.
     Steps:
     - Create an outer function that takes the array "priceList" as an argument.
     - In the scope of the outer function, sum all the value in array "priceList" 
@@ -11,24 +11,40 @@
     - Make appropriate calls to the outer and inner function and console log
     the value of "payablePrice".
     
-*/
+***************************************************************************/
 
 const priceList = [10.5, 9.9, 8.9, 4.5];
+let gstPrecent = 8;
 
-// Add code here
+function outerFunction(array) {
+    // Add code here
+    
+    return function(gst) {
+        // Add inner function code here
+    }
+console.log('Total price + GST =', outerFunction(priceList)(gstPercent));
+    
 
-/*
-    Task 2. Manipulate the "queue" array.
-*/
+/*************************************************************************
+    Task 2. Manipulate the "queue" array to add "start" and "last" in 
+            the suitable places in the array
+***************************************************************************/
 
 function createQueue(fn) {
   const queue = [1, 2, 3, 4];
-
   return fn(queue);
 }
 
-// Add code here - define the function that adds "start" and "last" to a given array
-
-const result = createQueue(); // Add code here - pass a reference of a function as an argument
+function doArray(array) {
+    // Add code here to manipulate the array
+    return array;
+}
+const result = ...
+    
+// Add code here - pass a reference of a function as an argument
 
 console.log(result); // Expected output: ["start", 1, 2, 3, 4, "last"];
+    
+/*************************************************************************
+    Extra challenge: convert the above solutions to arrow functions    
+ **************************************************************************/
