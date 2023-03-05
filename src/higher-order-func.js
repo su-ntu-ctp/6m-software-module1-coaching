abd63ac2e7951e6233f7d0a784e5cdef40d0027e
rkapril <rkapril@me.com>
2023-03-05 14:17:41 +0800

    updated typo in original js file

--- src/higher-order-func.js
+++ src/higher-order-func.js
@@ -18,17 +18,12 @@ let gstPercent = 8;
 
 function outerFunction(array) {
   // Add code here
-  let totalPrice = 0;
-  array.forEach((each) => {
-    totalPrice += each;
-  });
+
   return function (gst) {
     // Add inner function code here
-    let payablePrice = totalPrice * (gst / 100);
-    return "$" + payablePrice.toFixed(2);
   };
+  console.log("Total price + GST =", outerFunction(priceList)(gstPercent));
 }
-console.log("Total price + GST =", outerFunction(priceList)(gstPercent));
 
 /*************************************************************************
     Task 2. Manipulate the "queue" array to add "start" and "last" in 
@@ -42,11 +37,9 @@ function createQueue(fn) {
 
 function doArray(array) {
   // Add code here to manipulate the array
-  array.unshift("start");
-  array.push("last");
   return array;
 }
-const result = createQueue(doArray);
+const result = "...";
 
 // Add code here - pass a reference of a function as an argument
 
@@ -55,35 +48,3 @@ console.log(result); // Expected output: ["start", 1, 2, 3, 4, "last"];
 /*************************************************************************
     Extra challenge: convert the above solutions to arrow functions    
  **************************************************************************/
-
-let outerfunction = (array) => {
-  let totalPrice = 0;
-  array.forEach((each) => {
-    totalPrice += each;
-  });
-  return (gst) => {
-    let payablePrice = totalPrice * (gst / 100);
-    return "$" + payablePrice.toFixed(2);
-  };
-};
-
-console.log("Total price + GST =", outerFunction(priceList)(gstPercent));
-
-/**************************************************************************/
-
-let createQueue1 = (fn) => {
-  const queue = [1, 2, 3, 4];
-  return fn(queue);
-};
-
-let doArray1 = (array) => {
-  // Add code here to manipulate the array
-  array.unshift("start");
-  array.push("last");
-  return array;
-};
-const result1 = createQueue1(doArray1);
-
-// Add code here - pass a reference of a function as an argument
-
-console.log(result1); // Expected output: ["start", 1, 2, 3, 4, "last"];
